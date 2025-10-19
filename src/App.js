e496aeee8aa30e4e21c3fc795c60951d5b81e9b5
Wahab Ali Khan <wahab-ali-khan@hotmail.com>
2025-10-19 21:43:47 +0100

    first commit i.e. vibe coding my portfolio

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+export default function App() {
+  const [currentPage, setCurrentPage] = useState('home');
+  const [activeTab, setActiveTab] = useState('designer');
+
+  return (
+    <div>
+      <p>helloo there</p>
+    </div>
+  );
+}
